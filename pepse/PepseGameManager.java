d0de0fcd7333acadca2495e2c31629f2f1e8fe94
rotem <rotem@student.unsw.edu.au>
2025-05-31 22:35:00 +0300

    with terrein

--- pepse/PepseGameManager.java
+++ pepse/PepseGameManager.java
@@ -6,7 +6,9 @@ import danogl.gui.ImageReader;
 import danogl.gui.SoundReader;
 import danogl.gui.UserInputListener;
 import danogl.gui.WindowController;
+import pepse.world.Block;
 import pepse.world.Sky;
+import pepse.world.Terrain;
 
 public class PepseGameManager extends GameManager {
 	public static void main(String[] args) {
@@ -17,5 +19,9 @@ public class PepseGameManager extends GameManager {
 	public void initializeGame(ImageReader imageReader, SoundReader soundReader, UserInputListener inputListener, WindowController windowController) {
 		super.initializeGame(imageReader, soundReader, inputListener, windowController);
 		gameObjects().addGameObject(Sky.create(windowController.getWindowDimensions()), Layer.BACKGROUND);
+		Terrain terrain=new Terrain(windowController.getWindowDimensions(),1);
+		for (Block b:terrain.createInRange(0,(int)windowController.getWindowDimensions().x())){
+			gameObjects().addGameObject(b,Layer.DEFAULT);
+		}
 	}
 }
--- pepse/world/Terrain.java
+++ pepse/world/Terrain.java
@@ -1,4 +1,46 @@
 package pepse.world;
 
+import danogl.gui.rendering.RectangleRenderable;
+import danogl.gui.rendering.Renderable;
+import danogl.util.Vector2;
+import pepse.util.ColorSupplier;
+import pepse.util.NoiseGenerator;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Terrain {
+	public float groundHeightAtX0;
+	public NoiseGenerator noiseGenerator;
+	private static final Color BASE_GROUND_COLOR = new Color(212, 123,
+			74);
+	private static final int TERRAIN_DEPTH = 20;
+	public Terrain(Vector2 windowDimensions, int seed){
+		groundHeightAtX0=windowDimensions.y()*2/3;
+		noiseGenerator=new NoiseGenerator(seed,(int)groundHeightAtX0);
+
+
+	}
+	public float groundHeightAt(float x) { float noise = (float) noiseGenerator.noise(x, Block.SIZE *7); return groundHeightAtX0 + noise; }
+
+	public List<Block> createInRange(int minX, int maxX) {
+		int blockSize=Block.SIZE;
+		minX=(minX/blockSize)*blockSize;
+		maxX=(maxX/blockSize)*blockSize;
+		List<Block> lst = new ArrayList<>();
+		for(int x=minX; x<=maxX; x+=blockSize){
+			int y=(int)Math.floor(groundHeightAt(x)/blockSize)*blockSize;
+			for(int j=0; j<TERRAIN_DEPTH; j++){
+				Renderable rend = new RectangleRenderable(ColorSupplier.approximateColor(BASE_GROUND_COLOR));
+				Vector2 v = new Vector2(x, y+j*blockSize);
+				Block block = new Block(v, rend);
+				block.setTag("ground");
+				lst.add(block);
+
+			}
+
+		}
+		return lst;
+	}
 }
