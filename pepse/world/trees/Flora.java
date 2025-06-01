af615ecb833b9a1f3c969050740ff701c320a1c8
rotem <rotem@student.unsw.edu.au>
2025-06-01 12:18:54 +0300

    with all the flora

--- pepse/world/trees/Flora.java
+++ pepse/world/trees/Flora.java
@@ -1,9 +1,11 @@
 package pepse.world.trees;
 
 import danogl.GameObject;
+import danogl.components.ScheduledTask;
 import danogl.gui.rendering.RectangleRenderable;
 import danogl.gui.rendering.OvalRenderable;
 import danogl.util.Vector2;
+import pepse.util.Transition;
 import pepse.world.Block;
 import pepse.world.Terrain;
 
@@ -18,54 +20,131 @@ public class Flora {
 	private static final Color LEAF_COLOR = new Color(50, 200, 30);
 	private static final int BLOCK_SIZE = Block.SIZE;
 	private static final int LEAF_SIZE = BLOCK_SIZE;
+	private static final float LEAF_SIZE_CHANGE = 1.1f;
+	private static final float LEAF_ANGLE_CHANGE = 10f;
 	private static final int TRUNK_WIDTH = BLOCK_SIZE;
-	private static final int MAX_TRUNK_HEIGHT = 5;
+	private static final int MAX_TRUNK_HEIGHT = 6;
 	private static final int MIN_TRUNK_HEIGHT = 3;
 	private static final float TREES_PROB = 0.1f;
-
+	private static final float LEAF_PROB = 0.7f;
+	private static final String  LEAF_TAG = "leaf";
+	private static final String  TRUNK_TAG = "trunk";
 	private static final Random random = new Random();
 	private Terrain terrain;
 
+	/**
+	 * constructor that just use the terrain
+	 * @param terrain .
+	 */
 	public Flora(Terrain terrain) {
 		this.terrain = terrain;
 	}
 
-
+	/**
+	 * method that get a range between minx to maxX,
+	 * create all the flora on this area, and return in as list of objects
+	 * @param minX int
+	 * @param maxX int
+	 * @return list of objects
+	 */
 	public List<GameObject> createInRange(int minX, int maxX) {
 		List<GameObject> treeParts = new ArrayList<>();
-		minX=(minX/BLOCK_SIZE)*BLOCK_SIZE;
-		maxX=(maxX/BLOCK_SIZE)*BLOCK_SIZE;
+		minX = (minX / BLOCK_SIZE) * BLOCK_SIZE;
+		maxX = (maxX / BLOCK_SIZE) * BLOCK_SIZE;
 		for (int x = minX; x < maxX; x += BLOCK_SIZE) {
 			if (random.nextFloat() > TREES_PROB) continue; // create trees sparsely
 
 			float groundHeight = terrain.groundHeightAt(x);
-			int trunkHeight = MIN_TRUNK_HEIGHT + random.nextInt(MAX_TRUNK_HEIGHT - MIN_TRUNK_HEIGHT);
+			int trunkHeight = MIN_TRUNK_HEIGHT + random.nextInt(MAX_TRUNK_HEIGHT+1 - MIN_TRUNK_HEIGHT);
 
 			// Create trunk
 			for (int i = 1; i <= trunkHeight; i++) {
 				Vector2 pos = new Vector2(x, groundHeight - Block.SIZE * i);
 				GameObject trunkBlock = new Block(pos, new RectangleRenderable(TRUNK_COLOR));
-				trunkBlock.setTag("trunk");
+				trunkBlock.setTag(TRUNK_TAG);
 				treeParts.add(trunkBlock);
 			}
 
-			// Create leaves in a square
+			// Create leaves in a square of size commpared to the height of the tree.
+			float xPos,yPos;
 			float leafStartY = groundHeight - Block.SIZE * trunkHeight;
-			for (int dx = -1; dx <= 1; dx++) {
-				for (int dy = -1; dy <= 1; dy++) {
-					Vector2 leafPos = new Vector2(x + dx * LEAF_SIZE, leafStartY + dy * LEAF_SIZE);
-					GameObject leaf = new GameObject(leafPos, new Vector2(LEAF_SIZE, LEAF_SIZE),
-							new OvalRenderable(LEAF_COLOR));
-					leaf.setTag("leaf");
-					treeParts.add(leaf);
-
-					// TODO: Add Transition here to make leaf sway or pulse
+			for (int dx = -trunkHeight/2; dx <= trunkHeight/2; dx++) {
+				for (int dy = -trunkHeight/2; dy <= trunkHeight/2; dy++) {
+					xPos=x + dx * LEAF_SIZE;
+					yPos=leafStartY + dy * LEAF_SIZE;
+					Vector2 leafPos = new Vector2(xPos,yPos );
+					// create fruits where there are no leaves in high prob
+					if (random.nextFloat() > LEAF_PROB) {
+						if (random.nextFloat() < LEAF_PROB){
+							treeParts.add(FruitFactory.createFruit(leafPos));
+						}
+					}
+					else {
+						GameObject leaf = new GameObject(leafPos, new Vector2(LEAF_SIZE, LEAF_SIZE),
+								new RectangleRenderable(LEAF_COLOR));
+						leaf.setTag(LEAF_TAG);
+						animateLeafWithSwing(leaf);
+						animateLeafWithSizeChange(leaf);
+						treeParts.add(leaf);
+					}
 				}
 			}
-
-			// TODO: Add fruit on top of leaves with small chance
 		}
-
 		return treeParts;
+	}
+
+
+	public static void animateLeafWithSwing(GameObject leaf) {
+		float delay = random.nextFloat(); // random deley
+		float interval = 0.02f; // the animation interval
+		float cycleLength = 2f; // the full cycle of the animation
+
+		float[] t = {random.nextFloat()};
+		boolean[] forward = {true};
+
+		new ScheduledTask(leaf, delay, false, () -> {
+			new ScheduledTask(leaf, interval, true, () -> {
+				t[0] += interval / cycleLength;
+				if (t[0] >= 1f) {
+					t[0] = 0f;
+					forward[0] = !forward[0];
+				}
+
+				Transition.updateAngle(
+						t[0],
+						(angle, ignoredForward) -> leaf.renderer().setRenderableAngle(angle),
+						-LEAF_ANGLE_CHANGE,
+						-LEAF_ANGLE_CHANGE,
+						forward[0]
+				);
+			});
+		});
+	}
+
+	public static void animateLeafWithSizeChange (GameObject leaf) {
+		float delay = random.nextFloat() * 1f;
+		float interval = 0.02f;
+		float cycleLength = 2f;
+
+		float[] t = {random.nextFloat()};
+		boolean[] forward = {true};
+
+		new ScheduledTask(leaf, delay, false, () -> {
+			new ScheduledTask(leaf, interval, true, () -> {
+				t[0] += interval / cycleLength;
+				if (t[0] >= 1f) {
+					t[0] = 0f;
+					forward[0] = !forward[0];
+				}
+
+				Transition.updateSize(
+						t[0],
+						(size, ignoredForward) -> leaf.setDimensions(size),
+						new Vector2(LEAF_SIZE/LEAF_SIZE_CHANGE,LEAF_SIZE/LEAF_SIZE_CHANGE),
+						new Vector2(LEAF_SIZE*LEAF_SIZE_CHANGE, LEAF_SIZE*LEAF_SIZE_CHANGE),
+						forward[0]
+				);
+			});
+		});
 	}
 }
---
+++ pepse/world/trees/Transition.java
@@ -0,0 +1,74 @@
+package pepse.util;
+
+import danogl.util.Vector2;
+
+import java.util.function.BiConsumer;
+
+public class Transition {
+
+	/**
+	 * gives us an update for the angle, depend on going backward or forword
+	 * @param t where we at
+	 * @param onUpdate the lambda
+	 * @param min - min angle
+	 * @param max - max angle
+	 * @param forward - boolean
+	 */
+	public static void updateAngle(float t,
+								   BiConsumer<Float, Boolean> onUpdate,
+								   float min,
+								   float max,
+								   boolean forward) {
+		float interp = forward ? t : 1 - t;
+		float value = getValBetween(min, max, interp);
+		onUpdate.accept(value, forward);
+	}
+
+	/**
+	 * gives us an update for the fade, depend on going backward or forword
+	 * @param t where we at
+	 * @param onUpdate the lambda
+	 * @param min - min angle
+	 * @param max - max angle
+	 * @param forward - boolean
+	 */
+	public static void updateFade(float t,
+								  BiConsumer<Float, Boolean> onUpdate,
+								  float min,
+								  float max,
+								  boolean forward) {
+		float interp = forward ? t : 1 - t;
+		float value = getValBetween(min, max, interp);
+		onUpdate.accept(value, forward);
+	}
+
+	/**
+	 * update the size for setDimentions
+	 * @param t val
+	 * @param onUpdate the func lambda
+	 * @param min -min
+	 * @param max - max
+	 * @param forward -bool
+	 */
+	public static void updateSize(float t,
+								  BiConsumer<Vector2, Boolean> onUpdate,
+								  Vector2 min,
+								  Vector2 max,
+								  boolean forward) {
+		float interp = forward ? t : 1 - t;
+		float x = getValBetween(min.x(), max.x(), interp);
+		float y = getValBetween(min.y(), max.y(), interp);
+		onUpdate.accept(new Vector2(x, y), forward);
+	}
+
+	/**
+	 * calculate the advance value in compare to min and max
+	 * @param a - min
+	 * @param b -max
+	 * @param t - value between 0 to 1
+	 * @return
+	 */
+	private static float getValBetween(float a, float b, float t) {
+		return a + t * (b - a);
+	}
+}
